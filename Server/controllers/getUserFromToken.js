ac1a974bb14e85ce24351f5177014063856c3b8c
Martin Terniak <mterniak@gmail.com>
2024-03-28 10:09:57 +0200

    commit 10:10

--- Server/controllers/getUserFromToken.js
+++ Server/controllers/getUserFromToken.js
@@ -14,10 +14,10 @@ export const getUserFromToken = async (req, res, next) => {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     // Extract the user's ID from the token payload
-    const userId = decoded.id;
+    const id = decoded.id;
 
     // Retrieve the user from the database using the user's ID
-    const user = await User.findById(userId);
+    const user = await User.findById(id);
 
     if (!user) {
       return res.status(401).json({ message: "User not found" });
